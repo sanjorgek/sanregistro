8eb9dbf65d9a954d9b0794c9cf6945c6e6080620
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2016-06-14 21:00:37 -0500

    obtener una busqueda

--- index.js
+++ index.js
@@ -2,27 +2,41 @@ var async = require('async'),
   debug = require('debug')('register'),
   MongoClient = require('mongodb').MongoClient;
   
- function init(dburl, dbname) {
-   return {
-     register: function (person, cb) {
-       async.waterfall([
-         function(next){
-           MongoClient.connect(dburl,next);
-         },
-         function(db,next){
-           db.collection(dbname, next);
-         },
-         function(collection, next){
-           collection.find({email: person.email}).toArray(function(error, item){
-             next(error || (item.length>=1),collection);
-           });
-         },
-         function(collection, next) {
-           collection.insert(person, {safe: true}, next);
-         }
-       ], cb);       
-     }
-   }
- }
+function init(dburl, dbname) {
+  return {
+    register: function (person, cb) {
+      async.waterfall([
+        function(next){
+          MongoClient.connect(dburl,next);
+        },
+        function(db,next){
+          db.collection(dbname, next);
+        },
+        function(collection, next){
+          collection.find({email: person.email}).toArray(function(error, item){
+            next(error || (item.length>=1),collection);
+          });
+        },
+        function(collection, next) {
+          collection.insert(person, {safe: true}, next);
+        }
+      ], cb);       
+    },
+     
+    getApplicants: function (query, sort, cb) {
+      async.waterfall([
+        function (next) {
+          MongoClient.connect(dburl, next);
+        },
+        function (db, next) {
+          db.collection(dbname, next);
+        },
+        function (collection, next) {
+          collection.find(query).sort(sort).toArray(next);
+        }
+      ], cb);
+    }
+  }
+}
  
- module.exports = init;
+module.exports = init;
